4c484e65c51129e9d124377cc7f93dca12dbdfa3
Marcus Edralin <medralin36@yahoo.com>
2021-02-04 14:15:54 -0800

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import CampsiteInfo from './CampsiteInfoComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites.js';
 import { COMMENTS } from '../shared/comments';
@@ -31,13 +32,24 @@ class Main extends Component {
                 partner={this.state.partners.filter(partner => partner.featured)[0]}
                 />
             );
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return(
+                <CampsiteInfo 
+                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                />
+            );
         };
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
