a792966a842a978a73aef078c89c900316152c5a
Marcus Edralin <medralin36@yahoo.com>
2021-02-17 08:38:03 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -31,7 +31,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -95,7 +95,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
         if(comments) {
             return (
                 <div className = "col-md-5 m-1">
@@ -108,7 +108,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                             </div>
                         )
                     })}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             )
         }
@@ -169,7 +169,7 @@ function CampsiteInfo(props) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment} 
+                        postComment={props.postComment} 
                         campsiteId={props.campsite.id}
                         />
                     </div>
