8b5ba698b661b27eceff75a5bd89c66bbfe11260
Marcus Edralin <medralin36@yahoo.com>
2021-02-18 08:50:34 -0800

    React Animation Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const required = val => val && val.length;
@@ -100,14 +101,18 @@ function RenderComments({comments, postComment, campsiteId}) {
             return (
                 <div className = "col-md-5 m-1">
                     <h4>Comments</h4>
-                    {comments.map(comment => {
-                        return (
-                            <div key={comment.id}>
-                                <p>{comment.text}</p>
-                                <p>{comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                            </div>
-                        )
-                    })}
+                    <Stagger in>
+                        {comments.map(comment => {
+                            return (
+                                <Fade in key={comment.id}>
+                                    <div>
+                                        <p>{comment.text}</p>
+                                        <p>{comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    </div>
+                                </Fade>
+                            )
+                        })}
+                    </Stagger>
                     <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             )
@@ -117,12 +122,18 @@ function RenderComments({comments, postComment, campsiteId}) {
 function RenderCampsite({campsite}) {
         return(
             <div className = "col-md-5 m-1">
-                <Card>
-                    <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform 
+                    in 
+                    transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                    <Card>
+                        <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardText>{campsite.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
             </div>
         )
     }
